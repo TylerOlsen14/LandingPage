126632d5340b7296f76b8a4fd2ad89d55414801d
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-07-11 10:33:29 -0600

    added assets & code to change background based on time

--- js/main.js
+++ js/main.js
@@ -18,10 +18,36 @@ function showTime() {
   hour = hour % 12 || 12;
 
   //Output time
-  time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
   setTimeout(showTime, 1000);
 }
 
+// Add Zero
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+// Set Background and Greeting
+function setBGGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+  if(hour < 12){
+    // Moringing
+    document.body.style.backgroundImage = "url('../assets/morning.jpg')";
+    greeting.textContent = 'Good Morning';
+  } else if(hour < 18){
+    // Afternoon
+    document.body.style.backgroundImage = "url('../assets/afternoon.jpg')";
+    greeting.textContent = 'Good Afternoon';
+  } else {
+    // Evening
+    document.body.style.backgroundImage = "url('../assets/evening.jpg')";
+    greeting.textContent = 'Good Evening';
+    document.body.style.color = 'white'
+  }
+}
+
 // Run
 showTime();
+setBGGreet();
